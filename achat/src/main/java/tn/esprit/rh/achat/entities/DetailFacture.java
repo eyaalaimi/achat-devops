5d2ad166f285e66aeee8febc51509ab00de6940c
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:47:11 +0100

    Refactor DetailFacture class for consistency

--- achat/src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
@@ -1,54 +1,83 @@
 package tn.esprit.rh.achat.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.io.Serializable;
-
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class DetailFacture implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idDetailFacture;
-	private Integer qteCommandee;
-	private float prixTotalDetail;
-	private Integer pourcentageRemise;
-	private float montantRemise;
-	@ManyToOne
-	private Produit produit;
-	@ManyToOne
-	@JsonIgnore
-	Facture facture;
-	 // Getters
-    public Long getIdDetailFacture() { return idDetailFacture; }
-    public Integer getQteCommandee() { return qteCommandee; }
-    public Float getPrixTotalDetail() { return prixTotalDetail; }
-    public Float getPourcentageRemise() { return pourcentageRemise; }
-    public Float getMontantRemise() { return montantRemise; }
-    public Facture getFacture() { return facture; }
-    public Produit getProduit() { return produit; }
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetailFacture;
+    
+    private Integer qteCommandee;
+    private Float prixTotalDetail;
+    private Float pourcentageRemise;
+    private Float montantRemise;
+    
+    @ManyToOne
+    private Facture facture;
+    
+    @ManyToOne
+    private Produit produit;
+
+    // Getters
+    public Long getIdDetailFacture() {
+        return idDetailFacture;
+    }
+
+    public Integer getQteCommandee() {
+        return qteCommandee;
+    }
+
+    public Float getPrixTotalDetail() {
+        return prixTotalDetail;
+    }
+
+    public Float getPourcentageRemise() {
+        return pourcentageRemise;
+    }
+
+    public Float getMontantRemise() {
+        return montantRemise;
+    }
+
+    public Facture getFacture() {
+        return facture;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
 
     // Setters
-    public void setIdDetailFacture(Long idDetailFacture) { this.idDetailFacture = idDetailFacture; }
-    public void setQteCommandee(Integer qteCommandee) { this.qteCommandee = qteCommandee; }
-    public void setPrixTotalDetail(Float prixTotalDetail) { this.prixTotalDetail = prixTotalDetail; }
-    public void setPourcentageRemise(Float pourcentageRemise) { this.pourcentageRemise = pourcentageRemise; }
-    public void setMontantRemise(Float montantRemise) { this.montantRemise = montantRemise; }
-    public void setFacture(Facture facture) { this.facture = facture; }
-    public void setProduit(Produit produit) { this.produit = produit; }
-}
+    public void setIdDetailFacture(Long idDetailFacture) {
+        this.idDetailFacture = idDetailFacture;
+    }
+
+    public void setQteCommandee(Integer qteCommandee) {
+        this.qteCommandee = qteCommandee;
+    }
+
+    public void setPrixTotalDetail(Float prixTotalDetail) {
+        this.prixTotalDetail = prixTotalDetail;
+    }
+
+    public void setPourcentageRemise(Float pourcentageRemise) {
+        this.pourcentageRemise = pourcentageRemise;
+    }
+
+    public void setMontantRemise(Float montantRemise) {
+        this.montantRemise = montantRemise;
+    }
+
+    public void setFacture(Facture facture) {
+        this.facture = facture;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
 }
