0fb5c93b7a7760dbd7393a260921c5599cade2c8
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 15:12:51 +0100

    Refactor StockRestController to use DTOs

--- achat/src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
+++ achat/src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
@@ -1,73 +1,84 @@
 package tn.esprit.rh.achat.controllers;
 
-
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.rh.achat.dto.StockDTO;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.services.IStockService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Api(tags = "Gestion des stocks")
 @RequestMapping("/stock")
-@CrossOrigin("*")
 public class StockRestController {
 
-	@Autowired
-	IStockService stockService;
+    private static final Logger log = LoggerFactory.getLogger(StockController.class);
 
-	// http://localhost:8089/SpringMVC/stock/retrieve-all-stocks
-	@GetMapping("/retrieve-all-stocks")
-	@ResponseBody
-	public List<Stock> getStocks() {
-    return stockService.retrieveAllStocks();
-	}
+    @Autowired
+    IStockService stockService;
 
-	// http://localhost:8089/SpringMVC/stock/retrieve-stock/8
-	@GetMapping("/retrieve-stock/{stock-id}")
-	@ResponseBody
-	public Stock retrieveStock(@PathVariable("stock-id") Long stockId) {
-		return stockService.retrieveStock(stockId);
-	}
+    private StockDTO convertToDTO(Stock entity) {
+        if (entity == null) return null;
+        return new StockDTO(
+            entity.getIdStock(),
+            entity.getLibelleStock(),
+            entity.getQte(),
+            entity.getQteMin()
+        );
+    }
 
-	// http://localhost:8089/SpringMVC/stock/add-stock
-	@PostMapping("/add-stock")
-	@ResponseBody
-	public Stock addStock(@RequestBody Stock s) {
-		Stock stock = stockService.addStock(s);
-		return stock;
-	}
+    private Stock convertToEntity(StockDTO dto) {
+        if (dto == null) return null;
+        Stock entity = new Stock();
+        entity.setIdStock(dto.getIdStock());
+        entity.setLibelleStock(dto.getLibelleStock());
+        entity.setQte(dto.getQte());
+        entity.setQteMin(dto.getQteMin());
+        return entity;
+    }
 
-	// http://localhost:8089/SpringMVC/stock/remove-stock/{stock-id}
-	@DeleteMapping("/remove-stock/{stock-id}")
-	@ResponseBody
-	public void removeStock(@PathVariable("stock-id") Long stockId) {
-		stockService.deleteStock(stockId);
-	}
+    @GetMapping("/retrieve-all-stocks")
+    @ResponseBody
+    public List<StockDTO> getStocks() {
+        return stockService.retrieveAllStocks()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 
-	// http://localhost:8089/SpringMVC/stock/modify-stock
-	@PutMapping("/modify-stock")
-	@ResponseBody
-	public Stock modifyStock(@RequestBody Stock stock) {
-		return stockService.updateStock(stock);
-	}
+    @GetMapping("/retrieve-stock/{stock-id}")
+    @ResponseBody
+    public StockDTO retrieveStock(@PathVariable("stock-id") Long stockId) {
+        return convertToDTO(stockService.retrieveStock(stockId));
+    }
 
-	/*
-	 * Spring Scheduler : Comparer QteMin tolérée (à ne pa dépasser) avec
-	 * Quantité du stock et afficher sur console la liste des produits inférieur
-	 * au stock La fct schédulé doit obligatoirement etre sans paramètres et
-	 * sans retour (void)
-	 */
-	// http://localhost:8089/SpringMVC/stock/retrieveStatusStock
-	// @Scheduled(fixedRate = 60000)
-	// @Scheduled(fixedDelay = 60000)
-	//@Scheduled(cron = "*/60 * * * * *")
-	//@GetMapping("/retrieveStatusStock")
-//	@ResponseBody
-//	public void retrieveStatusStock() {
-//		stockService.retrieveStatusStock();
-//	}
+    @PostMapping("/add-stock")
+    @ResponseBody
+    public StockDTO addStock(@RequestBody StockDTO stockDTO) {
+        Stock entity = convertToEntity(stockDTO);
+        Stock saved = stockService.addStock(entity);
+        log.info("Added stock with id: {}", saved.getIdStock());
+        return convertToDTO(saved);
+    }
 
+    @DeleteMapping("/remove-stock/{stock-id}")
+    @ResponseBody
+    public void removeStock(@PathVariable("stock-id") Long stockId) {
+        log.info("Deleting stock with id: {}", stockId);
+        stockService.deleteStock(stockId);
+    }
+
+    @PutMapping("/modify-stock")
+    @ResponseBody
+    public StockDTO modifyStock(@RequestBody StockDTO stockDTO) {
+        Stock entity = convertToEntity(stockDTO);
+        Stock updated = stockService.updateStock(entity);
+        log.info("Modified stock with id: {}", updated.getIdStock());
+        return convertToDTO(updated);
+    }
 }
