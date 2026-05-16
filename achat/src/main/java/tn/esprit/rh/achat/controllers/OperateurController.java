bb36d556d53ec75b818477874d4395b6d82855a7
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 12:43:59 +0100

    Simplify getOperateurs method implementation

--- achat/src/main/java/tn/esprit/rh/achat/controllers/OperateurController.java
+++ achat/src/main/java/tn/esprit/rh/achat/controllers/OperateurController.java
@@ -21,10 +21,8 @@ public class OperateurController {
 	@GetMapping("/retrieve-all-operateurs")
 	@ResponseBody
 	public List<Operateur> getOperateurs() {
-		List<Operateur> list = operateurService.retrieveAllOperateurs();
-		return list;
+    	return operateurService.retrieveAllOperateurs();
 	}
-
 	// http://localhost:8089/SpringMVC/operateur/retrieve-operateur/8
 	@GetMapping("/retrieve-operateur/{operateur-id}")
 	@ResponseBody
