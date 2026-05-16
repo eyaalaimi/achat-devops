7aa5cc02ad0394cce088982232d5970fcb2ba3c5
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 15:07:56 +0100

    Refactor SecteurActivite class and update field names

--- achat/src/main/java/tn/esprit/rh/achat/entities/SecteurActivite.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/SecteurActivite.java
@@ -1,34 +1,33 @@
 package tn.esprit.rh.achat.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class SecteurActivite implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idSecteurActivite;
-	private String codeSecteurActivite;
-	private String libelleSecteurActivite;
-	@ManyToMany(mappedBy="secteurActivites")
-	@JsonIgnore
-	private Set<Fournisseur> fournisseurs;
+public class SecteurActivite implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSecteurActivite;
+    
+    private String codeSecteur;
+    private String libelleSecteur;
+    
+    @ManyToMany(mappedBy = "secteurActivites")
+    private Set<Fournisseur> fournisseurs;
+
+    // Getters
+    public Long getIdSecteurActivite() { return idSecteurActivite; }
+    public String getCodeSecteur() { return codeSecteur; }
+    public String getLibelleSecteur() { return libelleSecteur; }
+    public Set<Fournisseur> getFournisseurs() { return fournisseurs; }
+
+    // Setters
+    public void setIdSecteurActivite(Long idSecteurActivite) { this.idSecteurActivite = idSecteurActivite; }
+    public void setCodeSecteur(String codeSecteur) { this.codeSecteur = codeSecteur; }
+    public void setLibelleSecteur(String libelleSecteur) { this.libelleSecteur = libelleSecteur; }
+    public void setFournisseurs(Set<Fournisseur> fournisseurs) { this.fournisseurs = fournisseurs; }
 }
