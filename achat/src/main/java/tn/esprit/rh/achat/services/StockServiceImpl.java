b260a607cd2af4ac5dd2be5ea7ea28695ce5f857
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 16:59:46 +0100

    Update StockServiceImpl.java

--- achat/src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
+++ achat/src/main/java/tn/esprit/rh/achat/services/StockServiceImpl.java
@@ -1,89 +1,56 @@
 package tn.esprit.rh.achat.services;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
-@Slf4j
 public class StockServiceImpl implements IStockService {
 
-	@Autowired
-	StockRepository stockRepository;
+    private static final Logger log = LoggerFactory.getLogger(StockServiceImpl.class);
 
-	private static final Logger log = LoggerFactory.getLogger(StockServiceImpl.class);
-	@Override
-	public List<Stock> retrieveAllStocks() {
-		// récuperer la date à l'instant t1
-		log.info("In method retrieveAllStocks");
-		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
-		for (Stock stock : stocks) {
-			log.info(" Stock : " + stock);
-		}
-		log.info("out of method retrieveAllStocks");
-		// récuperer la date à l'instant t2
-		// temps execution = t2 - t1
-		return stocks;
-	}
+    @Autowired
+    StockRepository stockRepository;
 
-	@Override
-	public Stock addStock(Stock s) {
-		// récuperer la date à l'instant t1
-		log.info("In method addStock");
-		return stockRepository.save(s);
-		
-	}
+    @Override
+    public List<Stock> retrieveAllStocks() {
+        log.info("In method retrieveAllStocks");
+        List<Stock> stocks = stockRepository.findAll();
+        for (Stock stock : stocks) {
+            log.info("Stock : {}", stock);
+        }
+        log.info("out of method retrieveAllStocks");
+        return stocks;
+    }
 
-	@Override
-	public void deleteStock(Long stockId) {
-		log.info("In method deleteStock");
-		stockRepository.deleteById(stockId);
+    @Override
+    public Stock addStock(Stock s) {
+        return stockRepository.save(s);
+    }
 
-	}
+    @Override
+    public Stock updateStock(Stock s) {
+        return stockRepository.save(s);
+    }
 
-	@Override
-	public Stock updateStock(Stock s) {
-		log.info("In method updateStock");
-		return stockRepository.save(s);
-	}
+    @Override
+    public Stock retrieveStock(Long id) {
+        log.info("In method retrieveStock");
+        Stock stock = stockRepository.findById(id).orElse(null);
+        log.info("Stock : {}", stock);
+        log.info("out of method retrieveStock");
+        return stock;
+    }
 
-	@Override
-	public Stock retrieveStock(Long stockId) {
-		long start = System.currentTimeMillis();
-		log.info("In method retrieveStock");
-		Stock stock = stockRepository.findById(stockId).orElse(null);
-		log.info("out of method retrieveStock");
-		 long elapsedTime = System.currentTimeMillis() - start;
-		log.info("Method execution time: " + elapsedTime + " milliseconds.");
-
-		return stock;
-	}
-
-	@Override
-	public String retrieveStatusStock() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		Date now = new Date();
-		String msgDate = sdf.format(now);
-		String finalMessage = "";
-		String newLine = System.getProperty("line.separator");
-		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
-		for (int i = 0; i < stocksEnRouge.size(); i++) {
-			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
-					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
-					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
-					+ newLine;
-
-		}
-		log.info(finalMessage);
-		return finalMessage;
-	}
-
+    @Override
+    public void deleteStock(Long id) {
+        log.info("In method deleteStock");
+        stockRepository.deleteById(id);
+        log.info("out of method deleteStock");
+    }
 }
