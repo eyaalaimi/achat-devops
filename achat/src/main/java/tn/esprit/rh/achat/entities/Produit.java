e94d50a6de38dd2227768cfbc27698dd0af00055
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:06:22 +0100

    Refactor Produit entity with new attributes

--- achat/src/main/java/tn/esprit/rh/achat/entities/Produit.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Produit.java
@@ -1,55 +1,31 @@
 package tn.esprit.rh.achat.entities;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Produit implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idProduit;
-	private String codeProduit;
-	private String libelleProduit;
-	private float prix;
-	@Temporal(TemporalType.DATE)
-	private Date dateCreation;
-	@Temporal(TemporalType.DATE)
-	private Date dateDerniereModification;
-	@ManyToOne
-	@JsonIgnore
-	private Stock stock;
-	@OneToMany(mappedBy = "produit")
-	@JsonIgnore
-	private Set<DetailFacture> detailFacture;
-	@ManyToOne
-	@JsonIgnore
-	private CategorieProduit categorieProduit;
-	
-
-
-	
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduit;
+    
+    private String code;
+    private String libelle;
+    private Float prixUnitaire;
+    private Integer quantite;
+    
+    @ManyToOne
+    private Stock stock;
 }
