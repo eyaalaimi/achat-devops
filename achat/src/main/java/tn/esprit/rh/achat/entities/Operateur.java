929cf4b96691aa7d994bcb8f796074e40c3dfa42
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:10:16 +0100

    Update Operateur.java

--- achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -18,6 +18,8 @@ import java.util.Set;
 @ToString
 public class Operateur implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idOperateur;
