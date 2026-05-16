4c533c42c4d18876c6aeb1e3ba87e3f93be58a46
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 12:30:18 +0100

    Simplify getFournisseurs method implementation

--- achat/src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
+++ achat/src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
@@ -21,9 +21,8 @@ public class FournisseurRestController {
 	@GetMapping("/retrieve-all-fournisseurs")
 	@ResponseBody
 	public List<Fournisseur> getFournisseurs() {
-		List<Fournisseur> fournisseurs = fournisseurService.retrieveAllFournisseurs();
-		return fournisseurs;
-	}
+    return fournisseurService.retrieveAllFournisseurs();
+}
 
 	// http://localhost:8089/SpringMVC/fournisseur/retrieve-fournisseur/8
 	@GetMapping("/retrieve-fournisseur/{fournisseur-id}")
