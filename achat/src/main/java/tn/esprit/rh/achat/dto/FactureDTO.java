1a131e606209fff3225c7764bd69307c7090d489
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 11:47:44 +0100

    Refactor FactureDTO by reordering getters and setters

--- achat/src/main/java/tn/esprit/rh/achat/dto/FactureDTO.java
+++ achat/src/main/java/tn/esprit/rh/achat/dto/FactureDTO.java
@@ -8,31 +8,20 @@ public class FactureDTO {
     private Double montantFacture;
     private Date dateFacture;
     private Boolean active;
-    
-    // Constructors
+
     public FactureDTO() {}
-    
-    public FactureDTO(Long idFacture, Double montantRemise, Double montantFacture, Date dateFacture, Boolean active) {
-        this.idFacture = idFacture;
-        this.montantRemise = montantRemise;
-        this.montantFacture = montantFacture;
-        this.dateFacture = dateFacture;
-        this.active = active;
-    }
-    
-    // Getters and Setters
+
+    // Getters
     public Long getIdFacture() { return idFacture; }
-    public void setIdFacture(Long idFacture) { this.idFacture = idFacture; }
-    
     public Double getMontantRemise() { return montantRemise; }
-    public void setMontantRemise(Double montantRemise) { this.montantRemise = montantRemise; }
-    
     public Double getMontantFacture() { return montantFacture; }
-    public void setMontantFacture(Double montantFacture) { this.montantFacture = montantFacture; }
-    
     public Date getDateFacture() { return dateFacture; }
-    public void setDateFacture(Date dateFacture) { this.dateFacture = dateFacture; }
-    
     public Boolean getActive() { return active; }
+
+    // Setters
+    public void setIdFacture(Long idFacture) { this.idFacture = idFacture; }
+    public void setMontantRemise(Double montantRemise) { this.montantRemise = montantRemise; }
+    public void setMontantFacture(Double montantFacture) { this.montantFacture = montantFacture; }
+    public void setDateFacture(Date dateFacture) { this.dateFacture = dateFacture; }
     public void setActive(Boolean active) { this.active = active; }
 }
