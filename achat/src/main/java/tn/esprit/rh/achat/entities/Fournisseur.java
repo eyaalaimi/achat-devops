ef598c76688007e2ac8809be347c74867ea17cfc
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:51:21 +0100

    Add DetailFournisseur relationship to Fournisseur

--- achat/src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -16,6 +16,10 @@ public class Fournisseur implements Serializable {
     private String code;
     private String libelle;
     
+    @OneToOne
+    @JoinColumn(name = "detail_fournisseur_id")
+    private DetailFournisseur detailFournisseur;
+    
     @OneToMany(mappedBy = "fournisseur")
     private Set<Facture> factures;
     
@@ -26,6 +30,7 @@ public class Fournisseur implements Serializable {
     public Long getIdFournisseur() { return idFournisseur; }
     public String getCode() { return code; }
     public String getLibelle() { return libelle; }
+    public DetailFournisseur getDetailFournisseur() { return detailFournisseur; }
     public Set<Facture> getFactures() { return factures; }
     public Set<SecteurActivite> getSecteurActivites() { return secteurActivites; }
 
@@ -33,6 +38,7 @@ public class Fournisseur implements Serializable {
     public void setIdFournisseur(Long idFournisseur) { this.idFournisseur = idFournisseur; }
     public void setCode(String code) { this.code = code; }
     public void setLibelle(String libelle) { this.libelle = libelle; }
+    public void setDetailFournisseur(DetailFournisseur detailFournisseur) { this.detailFournisseur = detailFournisseur; }
     public void setFactures(Set<Facture> factures) { this.factures = factures; }
     public void setSecteurActivites(Set<SecteurActivite> secteurActivites) { this.secteurActivites = secteurActivites; }
 }
