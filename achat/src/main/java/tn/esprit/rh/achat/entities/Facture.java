7f754b5b94b18b0a4c1af8b2d90a6fb26f3ce0f0
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 11:56:25 +0100

    Refactor Facture entity structure and fields
    Refactored Facture entity by updating fields and relationships.

--- achat/src/main/java/tn/esprit/rh/achat/entities/Facture.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Facture.java
@@ -1,48 +1,29 @@
 package tn.esprit.rh.achat.entities;
 
+import lombok.*;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Facture implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idFacture;
-	private float montantRemise;
-	private float montantFacture;
-	@Temporal(TemporalType.DATE)
-	private Date dateCreationFacture;
-	@Temporal(TemporalType.DATE)
-	private Date dateDerniereModificationFacture;
-	private Boolean archivee;
-	@OneToMany(mappedBy = "facture")
-	private Set<DetailFacture> detailsFacture;
-    @ManyToOne
-    @JsonIgnore
-    private Fournisseur fournisseur;
-    @OneToMany(mappedBy="facture")
-    @JsonIgnore
-    private Set<Reglement> reglements;
+    private static final long serialVersionUID = 1L;
 
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idFacture;
+    
+    private Float montantRemise;
+    private Float montantFacture;
+    private Date dateFacture;
+    private Boolean active;
+    
+    // relationships...
 }
