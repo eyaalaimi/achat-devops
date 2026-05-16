16ed36c7910ef1eed9c8a6deb264d191522d587d
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 16:46:59 +0100

    Simplify retrieveReglement methods

--- achat/src/main/java/tn/esprit/rh/achat/services/ReglementServiceImpl.java
+++ achat/src/main/java/tn/esprit/rh/achat/services/ReglementServiceImpl.java
@@ -29,20 +29,14 @@ public class ReglementServiceImpl implements IReglementService {
 
 	@Override
 	public Reglement retrieveReglement(Long id) {
-		Reglement reglement = reglementRepository.findById(id).orElse(null);
-		
-		return reglement;
-	}
+    return reglementRepository.findById(id).orElse(null);
+}
+
 
 	@Override
 	public List<Reglement> retrieveReglementByFacture(Long idFacture) {
-		List<Reglement> reglements= reglementRepository.retrieveReglementByFacture(idFacture);
-		return reglements;
-		
-//		ou bien(Sans JPQL)
-//		Facture f= factureRepository.findById(idFacture).get();
-//		return (List<Reglement>) f.getReglements();
-	}
+    return reglementRepository.retrieveReglementByFacture(idFacture);
+}
 
 	@Override
 	public float getChiffreAffaireEntreDeuxDate(Date startDate, Date endDate) {
