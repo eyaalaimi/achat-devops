edca44ee9171c5771bcd4707332eae08e292a0a9
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:08:56 +0100

    Add serialVersionUID to CategorieProduit
    Added serialVersionUID for CategorieProduit class.

--- achat/src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -19,6 +19,8 @@ import java.util.Set;
 @ToString
 public class CategorieProduit implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCategorieProduit;
