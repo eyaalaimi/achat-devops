cc583c65c154fa31601de187db577f3ea6dbb4b0
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:18:37 +0100

    Remove Lombok annotations and add manual getters/setters

--- achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -1,21 +1,10 @@
 package tn.esprit.rh.achat.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Operateur implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -30,4 +19,18 @@ public class Operateur implements Serializable {
     
     @ManyToMany(mappedBy = "operateurs")
     private Set<Facture> factures;
+
+    // Getters
+    public Long getIdOperateur() { return idOperateur; }
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
+    public String getPassword() { return password; }
+    public Set<Facture> getFactures() { return factures; }
+
+    // Setters
+    public void setIdOperateur(Long idOperateur) { this.idOperateur = idOperateur; }
+    public void setNom(String nom) { this.nom = nom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+    public void setPassword(String password) { this.password = password; }
+    public void setFactures(Set<Facture> factures) { this.factures = factures; }
 }
