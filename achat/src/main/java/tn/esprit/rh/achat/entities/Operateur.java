9785e7590f1cc7960f990d469b037c0a80737a3a
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:05:53 +0100

    Refactor Operateur class with ManyToMany relationship

--- achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -1,38 +1,31 @@
 package tn.esprit.rh.achat.entities;
 
-import java.io.Serializable;
-import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Operateur implements Serializable{
-	private static final long serialVersionUID = 1L;
+@ToString
+public class Operateur implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idOperateur;
-	private String nom;
-	private String prenom;
-	
-	private String password;
-	@OneToMany
-	@JsonIgnore
-	private Set<Facture> factures;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOperateur;
+    
+    private String nom;
+    private String prenom;
+    private String password;
+    
+    @ManyToMany(mappedBy = "operateurs")
+    private Set<Facture> factures;
 }
