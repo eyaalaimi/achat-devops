283e56ff2a9233653599f041e0dd2f2b520653db
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:39:18 +0100

    Add Serializable interface and getters/setters

--- achat/src/main/java/tn/esprit/rh/achat/entities/DetailFournisseur.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/DetailFournisseur.java
@@ -14,6 +14,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -33,5 +35,19 @@ public class DetailFournisseur implements Serializable{
 	@OneToOne(mappedBy="detailFournisseur")
 	@JsonIgnore
 	private Fournisseur fournisseur;
+	// Getters
+    public Long getIdDetailFournisseur() { return idDetailFournisseur; }
+    public Date getDateDebutCollaboration() { return dateDebutCollaboration; }
+    public String getAdresse() { return adresse; }
+    public String getEmail() { return email; }
+    public Fournisseur getFournisseur() { return fournisseur; }
+
+    // Setters
+    public void setIdDetailFournisseur(Long idDetailFournisseur) { this.idDetailFournisseur = idDetailFournisseur; }
+    public void setDateDebutCollaboration(Date dateDebutCollaboration) { this.dateDebutCollaboration = dateDebutCollaboration; }
+    public void setAdresse(String adresse) { this.adresse = adresse; }
+    public void setEmail(String email) { this.email = email; }
+    public void setFournisseur(Fournisseur fournisseur) { this.fournisseur = fournisseur; }
+}	
 	
 }
