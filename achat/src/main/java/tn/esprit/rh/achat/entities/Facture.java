6a18d9f17c9c6a5d1ee0e7c63a36faa14e4c5e63
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:17:20 +0100

    Remove Lombok annotations and add manual getters/setters

--- achat/src/main/java/tn/esprit/rh/achat/entities/Facture.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Facture.java
@@ -1,23 +1,12 @@
 package tn.esprit.rh.achat.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Facture implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -42,4 +31,24 @@ public class Facture implements Serializable {
     @ManyToMany
     @JsonIgnore
     private Set<Operateur> operateurs;
+
+    // Getters
+    public Long getIdFacture() { return idFacture; }
+    public Float getMontantRemise() { return montantRemise; }
+    public Float getMontantFacture() { return montantFacture; }
+    public Date getDateFacture() { return dateFacture; }
+    public Boolean getActive() { return active; }
+    public Set<DetailFacture> getDetailFactures() { return detailFactures; }
+    public Fournisseur getFournisseur() { return fournisseur; }
+    public Set<Operateur> getOperateurs() { return operateurs; }
+
+    // Setters
+    public void setIdFacture(Long idFacture) { this.idFacture = idFacture; }
+    public void setMontantRemise(Float montantRemise) { this.montantRemise = montantRemise; }
+    public void setMontantFacture(Float montantFacture) { this.montantFacture = montantFacture; }
+    public void setDateFacture(Date dateFacture) { this.dateFacture = dateFacture; }
+    public void setActive(Boolean active) { this.active = active; }
+    public void setDetailFactures(Set<DetailFacture> detailFactures) { this.detailFactures = detailFactures; }
+    public void setFournisseur(Fournisseur fournisseur) { this.fournisseur = fournisseur; }
+    public void setOperateurs(Set<Operateur> operateurs) { this.operateurs = operateurs; }
 }
