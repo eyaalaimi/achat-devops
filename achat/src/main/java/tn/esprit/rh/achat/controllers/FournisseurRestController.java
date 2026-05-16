6cf56f1b1c84538c3c32b10464cfa5b391236153
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 12:34:36 +0100

    Refactor FournisseurRestController to use DTOs

--- achat/src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
+++ achat/src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
@@ -3,60 +3,77 @@ package tn.esprit.rh.achat.controllers;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.rh.achat.dto.FournisseurDTO;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.services.IFournisseurService;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @RestController
-@Api(tags = "Gestion des fournisseurss")
+@Api(tags = "Gestion des fournisseurs")
 @RequestMapping("/fournisseur")
 public class FournisseurRestController {
 
-	@Autowired
-	IFournisseurService fournisseurService;
+    @Autowired
+    IFournisseurService fournisseurService;
 
-	// http://localhost:8089/SpringMVC/fournisseur/retrieve-all-fournisseurs
-	@GetMapping("/retrieve-all-fournisseurs")
-	@ResponseBody
-	public List<Fournisseur> getFournisseurs() {
-    return fournisseurService.retrieveAllFournisseurs();
-}
+    // Convert Entity to DTO
+    private FournisseurDTO convertToDTO(Fournisseur entity) {
+        if (entity == null) return null;
+        return new FournisseurDTO(
+            entity.getIdFournisseur(),
+            entity.getCode(),
+            entity.getLibelle(),
+            entity.getDateDebutCollaboration()
+        );
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/retrieve-fournisseur/8
-	@GetMapping("/retrieve-fournisseur/{fournisseur-id}")
-	@ResponseBody
-	public Fournisseur retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
-		return fournisseurService.retrieveFournisseur(fournisseurId);
-	}
+    // Convert DTO to Entity
+    private Fournisseur convertToEntity(FournisseurDTO dto) {
+        if (dto == null) return null;
+        Fournisseur entity = new Fournisseur();
+        entity.setIdFournisseur(dto.getIdFournisseur());
+        entity.setCode(dto.getCode());
+        entity.setLibelle(dto.getLibelle());
+        entity.setDateDebutCollaboration(dto.getDateDebutCollaboration());
+        return entity;
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/add-fournisseur
-	@PostMapping("/add-fournisseur")
-	@ResponseBody
-	public Fournisseur addFournisseur(@RequestBody Fournisseur f) {
-		Fournisseur fournisseur = fournisseurService.addFournisseur(f);
-		return fournisseur;
-	}
+    @GetMapping("/retrieve-all-fournisseurs")
+    @ResponseBody
+    public List<FournisseurDTO> getFournisseurs() {
+        return fournisseurService.retrieveAllFournisseurs()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/remove-fournisseur/{fournisseur-id}
-	@DeleteMapping("/remove-fournisseur/{fournisseur-id}")
-	@ResponseBody
-	public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
-		fournisseurService.deleteFournisseur(fournisseurId);
-	}
+    @GetMapping("/retrieve-fournisseur/{fournisseur-id}")
+    @ResponseBody
+    public FournisseurDTO retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+        return convertToDTO(fournisseurService.retrieveFournisseur(fournisseurId));
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/modify-fournisseur
-	@PutMapping("/modify-fournisseur")
-	@ResponseBody
-	public Fournisseur modifyFournisseur(@RequestBody Fournisseur fournisseur) {
-		return fournisseurService.updateFournisseur(fournisseur);
-	}
+    @PostMapping("/add-fournisseur")
+    @ResponseBody
+    public FournisseurDTO addFournisseur(@RequestBody FournisseurDTO fournisseurDTO) {
+        Fournisseur entity = convertToEntity(fournisseurDTO);
+        Fournisseur saved = fournisseurService.addFournisseur(entity);
+        return convertToDTO(saved);
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/assignSecteurActiviteToFournisseur/1/5
-		@PutMapping(value = "/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
-		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
-			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
-		}
+    @DeleteMapping("/remove-fournisseur/{fournisseur-id}")
+    @ResponseBody
+    public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+        fournisseurService.deleteFournisseur(fournisseurId);
+    }
 
+    @PutMapping("/modify-fournisseur")
+    @ResponseBody
+    public FournisseurDTO modifyFournisseur(@RequestBody FournisseurDTO fournisseurDTO) {
+        Fournisseur entity = convertToEntity(fournisseurDTO);
+        Fournisseur updated = fournisseurService.updateFournisseur(entity);
+        return convertToDTO(updated);
+    }
 }
