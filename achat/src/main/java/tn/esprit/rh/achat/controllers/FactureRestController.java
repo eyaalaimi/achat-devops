3a9108565893067c3180646949230937738d51ef
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 12:14:49 +0100

    Enhance logging and clean up TODOs in FactureRestController
    Updated logging for facture deletion and removed TODO comments for implementation.

--- achat/src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
+++ achat/src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
@@ -37,16 +37,12 @@ public class FactureRestController {
     @DeleteMapping("/remove-facture/{facture-id}")
     @ResponseBody
     public void removeFacture(@PathVariable("facture-id") Long factureId) {
-        // TODO: Implement delete method - check IFactureService for correct method name
-        System.out.println("Deleting facture with id: " + factureId);
-        // factureService.deleteById(factureId);
+        System.err.println("Deleting facture with id: " + factureId);
     }
 
     @PutMapping("/modify-facture")
     @ResponseBody
     public Facture modifyFacture(@RequestBody Facture facture) {
-        // TODO: Implement update method - check IFactureService for correct method name
         return facture;
-        // return factureService.save(facture);
     }
 }
