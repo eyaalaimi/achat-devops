f9215a8fa06200028afe33517b7950142e6fc48c
eyaalaimi <eya.alaimi@sesame.com.tn>
2026-05-16 13:19:04 +0100

    Remove Lombok annotations and add manual getters/setters

--- achat/src/main/java/tn/esprit/rh/achat/entities/Produit.java
+++ achat/src/main/java/tn/esprit/rh/achat/entities/Produit.java
@@ -1,20 +1,9 @@
 package tn.esprit.rh.achat.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Produit implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -35,4 +24,22 @@ public class Produit implements Serializable {
     @ManyToOne
     @JoinColumn(name = "categorie_produit_id")
     private CategorieProduit categorieProduit;
+
+    // Getters
+    public Long getIdProduit() { return idProduit; }
+    public String getCode() { return code; }
+    public String getLibelle() { return libelle; }
+    public Float getPrixUnitaire() { return prixUnitaire; }
+    public Integer getQuantite() { return quantite; }
+    public Stock getStock() { return stock; }
+    public CategorieProduit getCategorieProduit() { return categorieProduit; }
+
+    // Setters
+    public void setIdProduit(Long idProduit) { this.idProduit = idProduit; }
+    public void setCode(String code) { this.code = code; }
+    public void setLibelle(String libelle) { this.libelle = libelle; }
+    public void setPrixUnitaire(Float prixUnitaire) { this.prixUnitaire = prixUnitaire; }
+    public void setQuantite(Integer quantite) { this.quantite = quantite; }
+    public void setStock(Stock stock) { this.stock = stock; }
+    public void setCategorieProduit(CategorieProduit categorieProduit) { this.categorieProduit = categorieProduit; }
 }
